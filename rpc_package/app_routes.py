36d1af66ca60bb68c96ef2327e3baf87ee891d5c
AliAkbarAhmadiDaryab <sau.ahmadi@gmail.com>
2021-07-07 19:52:09 +0100

    complete the invalid input data.

--- rpc_package/app_routes.py
+++ rpc_package/app_routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect, request, jsonify
-from rpc_package import app, translation_obj
+from rpc_package import app, translation_obj, message_obj
 from rpc_package.forms import CreateUserForm, LoginForm
 from wtforms.fields import Label
 
@@ -9,17 +9,23 @@ def blank():
     return render_template('blank.html', language='en')
 
 
-@app.route("/create_new_user")
+@app.route("/create_new_user", methods=['GET', 'POST'])
 def create_new_user():
-    language = 'dari'
+    language = 'en'
     create_new_user_form = CreateUserForm()
+    if create_new_user_form.validate_on_submit():
+        flash(message_obj.create_new_user_save[language].format(create_new_user_form.employee_id.data), 'success')
+    else:
+        flash(message_obj.create_new_user_not[language], 'danger')
+        print("Hello world")
     create_new_user_form.employee_id.label.text = translation_obj.employee_id[language]
     create_new_user_form.password.label.text = translation_obj.password[language]
     create_new_user_form.confirm_password.label.text = translation_obj.confirm_password[language]
     create_new_user_form.user_role.label.text = translation_obj.user_role[language]
     create_new_user_form.submit.label.text = translation_obj.submit_user[language]
     return render_template('create_new_user.html', title='Create New User',
-                           form=create_new_user_form, language=language, translation=translation_obj)
+                           form=create_new_user_form, language=language, translation=translation_obj,
+                           message_obj=message_obj)
 
 
 @app.route("/user_login")
