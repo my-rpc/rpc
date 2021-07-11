c2cf19d7561d6a13be48bd15f67435f15ef95342
AliAkbarAhmadiDaryab <sau.ahmadi@gmail.com>
2021-07-11 20:14:56 +0100

    Improve the message readability.

--- rpc_package/static/js/rpc.js
+++ rpc_package/static/js/rpc.js
@@ -4,10 +4,10 @@ formValidateErrors = function(response){
     for(var i in messages) {
         if(messages[i].length > 1 ){
             for(var j in messages[i]){
-                ul += "<li>" + messages[i][j] + "</li>";
+                ul += "<li>" +j +": " + messages[i][j] + "</li>";
             }
         }else{
-            ul += "<li>" + messages[i] + "</li>";
+            ul += "<li>"+i +": " + messages[i] + "</li>";
         }
     }
     ul += "</ul>";
