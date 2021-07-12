d2c30eb68c82776622738ad0cdfea185da80a9ae
Sakina Yousufi <Sakina.yosufie@gmail.com>
2021-07-12 16:10:28 +0430

    add dashboard in english

--- rpc_package/app_routes.py
+++ rpc_package/app_routes.py
@@ -89,7 +89,7 @@ def login():
 
 @app.route("/add_employee", methods=['GET', 'POST'])
 def add_employee():
-    language = 'en'
+    language = 'dari'
     add_employee_form = EmployeeForm()
     add_employee_contact_form = EmployeeContactForm()
     if request.method == 'POST':
@@ -149,3 +149,66 @@ def add_employee():
     return render_template('add_employee.html', title='Add Employee',
                            form=add_employee_form, language=language,
                            translation=translation_obj, message_obj=message_obj, form_contact=add_employee_contact_form)
+@app.route("/employee_list", methods=['GET', 'POST'])
+def employee_list():
+    language = 'en'
+    add_employee_form = EmployeeForm()
+    add_employee_contact_form = EmployeeContactForm()
+    if request.method == 'POST':
+        if add_employee_form.validate_on_submit():
+            new_employee = Employees(
+                id=add_employee_form.employee_id.data,
+                name=add_employee_form.first_name.data,
+                lname=add_employee_form.last_name.data,
+                fname=add_employee_form.father_name.data,
+                gname=add_employee_form.grand_name.data,
+                name_english=add_employee_form.first_name_english.data,
+                lname_english=add_employee_form.last_name_english.data,
+                fname_english=add_employee_form.father_name_english.data,
+                gname_english=add_employee_form.grand_name_english.data,
+                birthday=add_employee_form.birthday.data,
+                tazkira=add_employee_form.tazkira.data,
+                gender=True if add_employee_form.gender.data else False,
+                blood=add_employee_form.blood.data,
+                m_status=True if add_employee_form.m_status.data else False,
+                tin=add_employee_form.tin.data,
+                status=1)
+            try:
+                db.session.add(new_employee)
+                db.session.commit()
+            except IOError as exc:
+                return jsonify({'success': False, 'message': message_obj.create_new_employee_not[language]}), \
+                       403, {'ContentType': 'application/json'}
+            return jsonify({'success': True, 'message':
+                message_obj.create_new_employee_save[language].format(add_employee_form.employee_id.data)}), \
+                   200, {'ContentType': 'application/json'}
+        elif add_employee_contact_form.validate_on_submit():
+            permanent_address = Permanent_addresses(emp_id=add_employee_contact_form.employee_id.data,
+                                              address=add_employee_contact_form.permanent_address.data,
+                                              district_id=add_employee_contact_form.district.data,
+                                              province_id=add_employee_contact_form.province.data
+                                              )
+            current_address = Current_addresses(emp_id=add_employee_contact_form.employee_id.data,
+                                              address=add_employee_contact_form.current_address.data,
+                                              district_id=add_employee_contact_form.district.data,
+                                              province_id=add_employee_contact_form.province.data
+                                              )
+            try:
+                db.session.add(permanent_address)
+                db.session.add(current_address)
+                db.session.commit()
+            except IOError as exc:
+                return jsonify({'success': False, 'message': message_obj.contact_details_not[language]}), \
+                       403, {'ContentType': 'application/json'}
+            return jsonify({'success': True, 'message': message_obj.contact_details[language]}), \
+                   200, {'ContentType': 'application/json'}
+
+        else:
+            return jsonify({'success': False, 'message': add_employee_form.errors}), \
+                   403, {'ContentType': 'application/json'}
+
+    add_employee_form = update_messages_employee(add_employee_form, language)
+    return render_template('employee_list.html', title='Add Employee',
+                           form=add_employee_form, language=language,
+                           translation=translation_obj, message_obj=message_obj, form_contact=add_employee_contact_form)
+
