50e4df2ba633026769c8eab4a0f67fd226788ea8
AliAkbarAhmadiDaryab <sau.ahmadi@gmail.com>
2021-07-26 23:59:01 +0100

    Update the upload file app functions.

--- rpc_package/route_utils.py
+++ rpc_package/route_utils.py
@@ -5,14 +5,18 @@ from rpc_package.rpc_tables import Documents
 
 
 def upload_docs(emp_id, request, file_type):
-    request_file = request.files[file_type]
-    request_file.filename = "Guarantor-" + emp_id + ".pdf"
-    path = os.path.join(f"./rpc_package/static/files/{file_type}", request_file.filename)
-    document = Documents(
-        emp_id=emp_id,
-        name="guarantor",
-        url="/static/files/guarantor/" + request_file.filename)
-    assert isinstance(db, object)
-    db.session.add(document)
-    db.session.commit()
-    request_file.save(path)
+    try:
+        request_file = request.files[file_type]
+        request_file.filename = f"{file_type}-" + emp_id + ".pdf"
+        path = os.path.join(f"./rpc_package/static/files/{file_type}", request_file.filename)
+        document = Documents(
+            emp_id=emp_id,
+            name=file_type,
+            url=f"/static/files/{file_type}/" + request_file.filename)
+        assert isinstance(db, object)
+        request_file.save(path)
+        db.session.add(document)
+        db.session.commit()
+        return 'success'
+    except IOError as io:
+        return 'error'
