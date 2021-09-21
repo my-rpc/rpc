42d35503c1b1a02714994ada460fee4086175a60
Mohammad Aziz Nabizada <49517524+aziznabizada@users.noreply.github.com>
2021-09-21 16:08:23 +0430

    aziz/feature/loan_form the jquery ui library added and the auto complete done for guarantor field in loan form

--- rpc_package/app_routes.py
+++ rpc_package/app_routes.py
@@ -793,6 +793,21 @@ def loan_request():
                            title=translation_obj.forms[session['language']], language=session['language'],
                            translation=translation_obj, message_obj=message_obj)
 
+@app.route('/emp_autocomplete', methods=['GET'])
+def emp_autocomplete():
+    search = request.args.get('q')
+    name = Employees.name
+    lname = Employees.lname
+    if session['language'] == 'en':
+        name = Employees.name_english
+        lname = Employees.lname_english
+    employees = db.session.query(Employees.id, name, lname).filter((Employees.id.like('%' + str(search) + '%') | Employees.name.like('%' + str(search) + '%') | Employees.lname.like('%' + str(search) + '%') | Employees.name_english.like('%' + str(search) + '%') | Employees.lname_english.like('%' + str(search) + '%')))
+    result = [({'value': mv[0], 'label': mv[0] + ' ' + mv[1] + ' ' + mv[2]}) for mv in employees.limit(10).all()]
+    message = ''
+    if not result :
+        message = translation_obj.not_found[session['language']]
+    return jsonify(result = result, message = message)
+
 @app.route('/loan_guarantor', methods=["GET"])
 @login_required
 def loan_guarantor():
